7721538ccfb55d45c04322d5ff91c6d1cad1b704
Sebastian Sikora <ssikora80@gmail.com>
2021-03-18 12:10:33 +0100

    Fix styles on mobile devices

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+/*
+// finde burger btn 
+const btnHamburger = document.querySelector('#btnHamburger');
+
+// add listener on burger btn
+btnHamburger.addEventListener('click', function(){
+    console.log('hamburger click');
+
+    // remove or add 'open' class 
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }
+    else {
+        btnHamburger.classList.add('open');
+    }
+});
+
+*/
