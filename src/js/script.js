7616e1dd47877681b89804496cb3c00a27389a4b
Sebastian Sikora <ssikora80@gmail.com>
2021-03-18 21:08:35 +0100

    Fix mobile device styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,36 @@
-/*
-// finde burger btn 
-const btnHamburger = document.querySelector('#btnHamburger');
+function burgerAcordion() {
+  // finde burger btn 
+  const btnHamburger = document.querySelector('#btnHamburger');
 
-// add listener on burger btn
-btnHamburger.addEventListener('click', function(){
+  // finde sidebar menu
+  const sidebarMenu = document.getElementById('sidebar');
+
+  /* [WORK IN PROGRESS]
+  const overlay = document.querySelector('#overlay');
+  */
+
+  // add listener on burger btn
+  btnHamburger.addEventListener('click', function(){
     console.log('hamburger click');
 
-    // remove or add 'open' class 
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
-    }
-    else {
-        btnHamburger.classList.add('open');
-    }
-});
+    // remove or add 'open' class to sidebar 
+    if(sidebarMenu.classList.contains('open')){ //Close
+        sidebarMenu.classList.remove('open');
 
-*/
+        /* [WORK IN PROGRESS]
+        overlay.classList.remove('fade-in'); 
+        overlay.classList.add('fade-out');
+        */
+    }
+    else { // Open
+        sidebarMenu.classList.add('open');
+        
+        /* [WORK IN PROGRESS]
+        //overlay.classList.remove('fade-out');
+        //overlay.classList.add('fade-in');
+        */
+    }
+  });
+}
+
+burgerAcordion();
